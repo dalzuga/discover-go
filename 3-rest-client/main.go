1fca126afe540122d78bd0c3041af3a45e5c426f
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-06 14:11:37 -0800

    printed JSON with pretty indent

--- 3-rest-client/main.go
+++ 3-rest-client/main.go
@@ -12,12 +12,13 @@ func main() {
 	timenow := time.Now()
 
 	respvar, err := http.Get("http://www.omdbapi.com/?i=tt0372784&plot=short&r=json")
+	defer respvar.Body.Close()
 
 	if err != nil {
 		fmt.Println("error1:", err)
 	}
 
-	fmt.Println("status code is", err, http.StatusOK)
+	fmt.Println("status code is", respvar.Status)
 
 	var respvarStruct JsonOmdbStruct
 
@@ -31,6 +32,23 @@ func main() {
 	// 	fmt.Println("can't marshal:", err)
 	// }
 
-	fmt.Printf("struct output: %+v\ntime: %v\n", respvarStruct, timenow)
-	// fmt.Printf("json output: %+v\ntime: %v\n", respvarMarshaled, timenow)
+	// respvarMarshaled, err := json.Marshal(&respvarStruct)
+	//
+	// if err != nil {
+	// 	fmt.Println("error3:", err)
+	// }
+
+	respvarMarshaledIndent, err := json.MarshalIndent(&respvarStruct, "", "  ")
+
+	if err != nil {
+		fmt.Println("error3:", err)
+	}
+
+	//fmt.Printf("respvar: %v\n", respvar.Body)
+	//fmt.Printf("jsonMarshaled:\n%s\n", respvarMarshaled)
+	fmt.Printf("jsonMarshaledIndent:\n%s\n", respvarMarshaledIndent)
+	//fmt.Printf("struct output: %+v\ntime: %v\n", respvarStruct, timenow)
+	//fmt.Printf("respvarStruct:\n%#v\n", respvarStruct)
+	fmt.Printf("time: %v\n", timenow)
+	//fmt.Printf("json output: %s\ntime: %v\n", respvarMarshaled, timenow)
 }
