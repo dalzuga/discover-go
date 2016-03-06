f5ff5ea1c4cfe4d3b690973cbd09dd665f3d75a7
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-06 13:04:44 -0800

    working on status code from GET

--- 3-rest-client/main.go
+++ 3-rest-client/main.go
@@ -14,14 +14,23 @@ func main() {
 	respvar, err := http.Get("http://www.omdbapi.com/?i=tt0372784&plot=short&r=json")
 
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("error1:", err)
 	}
 
-	respvarjson, err := json.Marshal(respvar)
+	fmt.Println("status code is", err, http.StatusOK)
 
-	if err != nil {
-		fmt.Println("error:", err)
+	var respvarStruct JsonOmdbStruct
+
+	if err := json.NewDecoder(respvar.Body).Decode(&respvarStruct); err != nil {
+		fmt.Println("error2:", err)
 	}
 
-	fmt.Printf("json output: %v\ntime: %v\n", respvarjson, timenow)
+	// respvarMarshaled, err := json.Marshal(respvarStruct)
+	//
+	// if err != nil {
+	// 	fmt.Println("can't marshal:", err)
+	// }
+
+	fmt.Printf("struct output: %+v\ntime: %v\n", respvarStruct, timenow)
+	// fmt.Printf("json output: %+v\ntime: %v\n", respvarMarshaled, timenow)
 }
