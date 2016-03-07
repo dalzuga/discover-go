db924307c84e58808ee6d2179510fb580ac85e82
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-06 16:14:37 -0800

    printed out some stuff and added tomato rating

--- 3-rest-client/main.go
+++ 3-rest-client/main.go
@@ -10,53 +10,40 @@ import (
 
 func main() {
 
-	//timenow := time.Now()
-
-	respvar, err := http.Get("http://www.omdbapi.com/?i=tt0372784&plot=short&r=json")
-	defer respvar.Body.Close()
-
-	if err != nil {
-		fmt.Println("error1:", err)
-	}
-
-	fmt.Println("status code is", respvar.Status)
-
-	var respvarStruct JsonOmdbStruct
-
-	if err := json.NewDecoder(respvar.Body).Decode(&respvarStruct); err != nil {
-		fmt.Println("error2:", err)
-	}
-
 	u, err := url.Parse("http://www.omdbapi.com/?i=tt0372784&plot=short&r=json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("The movie is '%s' and the actors are %s.\n", respvarStruct.Title, respvarStruct.Actors)
-
 	u.Scheme = "http"
 	u.Host = "omdbapi.com"
 	q := u.Query()
 	q.Set("i", "tt3682448")
+	q.Set("tomatoes", "true")
 	u.RawQuery = q.Encode()
 
 	address := u.String()
 
 	fmt.Printf("%s\n", address)
 
-	respvar, err = http.Get(address)
+	var respvarStruct JsonOmdbStruct
+
+	respvar, err := http.Get(address)
 	defer respvar.Body.Close()
 
 	if err != nil {
 		fmt.Println("error1:", err)
 	}
 
-	fmt.Println("status code is", respvar.Status)
-
-	if err = json.NewDecoder(respvar.Body).Decode(&respvarStruct); err != nil {
+	if err := json.NewDecoder(respvar.Body).Decode(&respvarStruct); err != nil {
 		fmt.Println("error2:", err)
 	}
 
-	fmt.Println("Parsed URL", u)
-	fmt.Printf("The movie is '%s' and the actors are %s.\n", respvarStruct.Title, respvarStruct.Actors)
+	fmt.Println("status code is", respvar.Status)
+
+	fmt.Println("The movie ID is", respvarStruct.ImdbID)
+	fmt.Println("The parsed URL is", u)
+	fmt.Printf("The movie name is '%s' and the actors are %s.\n", respvarStruct.Title, respvarStruct.Actors)
+	fmt.Printf("The IMBD rating is %s with %s votes.\n", respvarStruct.ImdbRating, respvarStruct.ImdbVotes)
+	fmt.Printf("The tomato rating is %s\n", respvarStruct.TomatoRating)
 }
